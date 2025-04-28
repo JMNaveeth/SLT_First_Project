9dc499e6f30ade798a3e94902eeb8a739c455575
JMNaveeth <naveethkinniya2001@gmail.com>
2025-04-28 17:58:08 +0530

    Refactor ViewBatteryUnit: Remove SearchHelperBattery class to streamline battery data querying

--- lib/viewBatterySystem/viewBatterySystem/ViewBatteryUnit.dart
+++ lib/viewBatterySystem/viewBatterySystem/ViewBatteryUnit.dart
@@ -140,18 +140,7 @@ class ViewBatteryUnitState extends State<ViewBatteryUnit> {
           ThemeToggleButton(), // Use the reusable widget
         ],
       ),
-      class SearchHelperBattery {
-  static bool matchesBatteryQuery(Map<String, dynamic> battery, String query) {
-    query = query.toLowerCase();
-    return _batteryMatchesQuery(battery, query);
-  }
-
-  static bool _batteryMatchesQuery(Map<String, dynamic> battery, String query) {
-    // Check all possible fields in the battery data
-    return battery.values.any((value) =>
-    value != null && value.toString().toLowerCase().contains(query));
-  }
-}
+      
       body:
            _dataLoaded
     ? SingleChildScrollView(
